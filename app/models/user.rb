c25d909fe8cd2ba923d8b1af85e2aa5bc9ae3872
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-03-26 13:32:16 -0400

    Setup user model and start routes

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :session_token, presence: true
+  validates :password, length: {minimum: 6}, allow: nil
+
+  after_initialize :ensure_session_token
+
+      #ASSOCIATIONS HERE#
+
+  def password=(password)
+    @password = password
+    this.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by({username: username})
+    return nil unless user && user.is_password(password)
+    user
+  end
+
+  def is_password(password)
+    bcrypt_password = BCrypt::Password.new(password_digest)
+    bcrypt_password.is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cage = User.create!({ username: "Nicolas Cage", email: "wild@heart.com", password: "notthebees"});
