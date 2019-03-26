650883a845702c5002a5c794b9f511795d94de72
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-03-26 16:56:17 -0400

    Prepare to push to heroku

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,33 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+
+export const login = (user) => dispatch => {
+  return SessionApiUtil.login(user).then((user) => dispatch(receiveCurrentUser(user)));
+};
+
+export const logout = () => dispatch => {
+  return SessionApiUtil.logout().then(() => dispatch(logoutCurrentUser()));
+};
+
+export const signup = (user) => dispatch => {
+  return SessionApiUtil.signup(user).then((user) => dispatch(receiveCurrentUser(user)));
+};
+
+
+export const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
