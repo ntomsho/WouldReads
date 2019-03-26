bcd22254230289d59d809ec1c6633929963f6a5d
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-03-26 14:11:08 -0400

    Add users_controller.rb

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,33 @@
+class Api::UsersController < ApplicationController
+
+  def show
+    @user = User.find_by(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+  
+  def update
+    @user = User.find_by(params[:id])
+    if @user.update(user_params)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
