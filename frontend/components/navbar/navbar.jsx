31fe33034fb8e2539f0b34bdbb6bd8824c5581ba
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-03-28 08:53:20 -0400

    Start minor styling on logged in placeholder

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -17,6 +17,8 @@ const Navbar = ({currentUser, logout}) => {
   const loggedInNavbar = () => {
     return (
     <div className="navbar-container">
+      <Link to="/"><img src="https://s.gr-assets.com/assets/home/header_logo-8d96d7078a3d63f9f31d92282fd67cf4.png"
+          className="navbar-logo" /></Link>
       <div className="loggedin-header">
         <h2 className="header-name">Placeholder {currentUser.username}</h2>
         <button className="header-placeholder-button" onClick={logout}>Log Out</button>
