536d704f35065cfd304690a4bc161c8ec3840e74
Nick <ntomsho4545@gmail.com>
2019-04-08 17:42:14 -0400

    Fix paragraph breaks in seed file

--- frontend/components/books/book_show_my_activity.jsx
+++ frontend/components/books/book_show_my_activity.jsx
@@ -8,39 +8,20 @@ class BookShowMyActivity extends React.Component {
         super(props)
     }
 
-    // shelvesByBook() {
-    //     const { book, shelves, reviews, user } = this.props;
-    //     debugger
-    //     let shelvesByBook;
-    //     if (shelves.length > 0) {
-    //         shelvesByBook = book.shelves.map(id => {
-    //             if (shelves[id].title !== "All") {
-    //                 return (
-    //                     <Link to={`/shelves/${id}`} className="shelf-show-link" key={shelves[id].id}>{shelves[id].title}</Link>
-    //                 );
-    //             }
-    //         });
-    //     }
-    //     debugger
-    //     return shelvesByBook;
-    // }
-
     render() {
         const {book, shelves, reviews, user} = this.props;
 
         let shelvesByBook;
         if (Object.keys(shelves).length > 0) {
-            debugger
             shelvesByBook = book.shelves.map(id => {
                 if (shelves[id].title !== "All") {
-                    debugger
                     return (
                         <Link to={`/shelves/${id}`} className="shelf-show-link" key={shelves[id].id}>{shelves[id].title}</Link>
                     );
                 }
             });
         }
-        debugger
+
         return (
         <>
         <div className="my-activity-header">MY ACTIVITY</div>
