48851c0555fd403743415a84a9c1bdd8c9386833
Nick <ntomsho4545@gmail.com>
2019-04-23 11:56:51 -0400

    Fix logger import in store.js

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
 if (process.env.NODE_ENV !== 'production') {
